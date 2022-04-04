b48361c4450c6a7d8ef8f8d06b8316c071845eb0
Erfan Ebrahimi <erfan.ebrahimi@energi.team>
2022-04-05 03:12:16 +0430

    initialize

---
+++ src/http/method.rs
@@ -0,0 +1,34 @@
+use std::str::FromStr;
+
+pub enum Method {
+  GET,
+  POST,
+  PUT,
+  PATCH,
+  HEAD,
+  CONNECT,
+  OPTIONS,
+  TRACE,
+  DELETE,
+}
+
+impl FromStr for Method {
+  type Err = MethodError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      "GET" => Ok(Self::GET),
+      "POST" => Ok(Self::POST),
+      "PUT" => Ok(Self::PUT),
+      "PATCH" => Ok(Self::PATCH),
+      "HEAD" => Ok(Self::HEAD),
+      "CONNECT" => Ok(Self::CONNECT),
+      "OPTIONS" => Ok(Self::OPTIONS),
+      "TRACE" => Ok(Self::TRACE),
+      "DELETE" => Ok(Self::DELETE),
+      _ => Err(MethodError)
+    }
+  }
+}
+
+pub struct MethodError; 
---
+++ src/server.rs
@@ -0,0 +1,39 @@
+use std::net::TcpListener;
+use crate::http::Request;
+use std::convert::{TryFrom, TryInto};
+use std::io::Read;
+
+pub struct Server {
+  addr: String,
+}
+
+impl Server {
+  pub fn new(addr: String) -> Self {
+    Self { addr }
+  }
+  pub fn run(self) {
+    println!("Listening on {}", self.addr);
+
+    let listener = TcpListener::bind(&self.addr).unwrap();
+
+    loop {
+      match listener.accept() {
+        Ok((mut stream, _)) => {
+          let mut buffer = [0; 8096];
+          match stream.read(&mut buffer) {
+            Ok(_) => {
+              println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+              match Request::try_from(&buffer[..]) {
+                Ok(request) => {},
+                Err(err) => println!("Failed to parse a request: {}", err)
+              }
+            }
+            Err(_) => println!("Failed to read from connection")
+          }
+        },
+        Err(e) => println!("Failed to establish a connection: {}", e)
+      }
+
+    }
+  }
+}
