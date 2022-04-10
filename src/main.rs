6305fc1c11f487b9f1682e4487b39a136a0b91f0
Erfan Ebrahimi <erfan.ebrahimi@energi.team>
2022-04-10 06:35:42 +0430

    Adding port to env

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ mod website_handler;
 
 fn main() {
     let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let default_port: String = String::from("8080");
     let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
-    let server = Server::new("127.0.0.1:8080".to_string());
+    let port = env::var("PORT").unwrap_or(default_port);
+    let server = Server::new(format!("127.0.0.1:{}", port));
     server.run(WebsiteHandler::new(public_path));
 }
